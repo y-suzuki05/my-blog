5cd41f61462b84e3ea66f34dc8687118be2172ed
yuki <momo_968_weekend@yahoo.co.jp>
2024-12-29 10:08:26 +0900

    Headerコンポーネント作成

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>記事一覧</h1>
+    </div>
+  );
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-background text-foreground">
+      <div className="container mx-auto max-w-7xl p-4 flex items-center justify-between">
+        <h1 className="text-2xl font-bold">szyk-tech-blog</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/posts"}>Posts</Link>
+            </li>
+            <li>
+              <Link href={"/profile"}>Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
